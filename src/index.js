138fc8fcc4bab11ae3da039e4bc82e1254b7bf4f
Dandara Estrela <31369111+Dandaraestrela@users.noreply.github.com>
2021-11-20 20:22:10 -0300

    Revert "files structure, eslint, prettier"

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './Routes';
-import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/global';
-import theme from './styles/theme';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Routes />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
