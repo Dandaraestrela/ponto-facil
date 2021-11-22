21876dd629b3a37e99aa0b9a1ea3d13d0d49b84d
Dandara Estrela <dandara_estrela@hotmail.com>
2021-11-22 09:32:30 -0300

    login page style[C

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,18 @@
 import * as S from './Login.styles';
 
+import LogoLarge from 'assets/images/LogoLarge.svg';
 import { Button } from 'components';
 
 const Login = () => {
   return (
     <S.StyledWrapper>
-      login
-      <Button>oi</Button>
+      <S.StyledContent>
+        <img src={LogoLarge} width="40%" />
+        <Button col={6} type="primary" onClick={() => {}}>
+          Login
+        </Button>
+      </S.StyledContent>
+      <S.StyledBG />
     </S.StyledWrapper>
   );
 };
--- src/pages/Login/Login.styles.js
+++ src/pages/Login/Login.styles.js
@@ -1,7 +1,31 @@
 import styled from 'styled-components';
+import LoginBG from '../../assets/images/LoginBG.svg';
 
 export const StyledWrapper = styled.div`
   display: flex;
   width: 100vw;
   height: 100vh;
+  overflow: auto;
+  background-color: ${({ theme: { colors } }) => colors.primary.lightest};
+`;
+
+export const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  height: 100%;
+  background-color: ${({ theme: { colors } }) => colors.neutral.white};
+  border-radius: 0px 40px 40px 0px;
+  box-shadow: 5px 8px 28px -6px rgba(24, 39, 75, 0.12),
+    5px 18px 88px -4px rgba(24, 39, 75, 0.14);
+  z-index: 1;
+`;
+
+export const StyledBG = styled.div`
+  width: 50%;
+  background-image: url(${LoginBG});
+  background-position: center;
+  background-repeat: no-repeat;
 `;
