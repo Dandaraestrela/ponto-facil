1f9529b1929914be8906601d8f1f425e1d62c2a0
Dandara Estrela <dandara_estrela@hotmail.com>
2021-11-24 14:50:53 -0300

    employees redux

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authReducer from './auth';
+import employeesReducer from './employees';
 
 const REDUCERS = combineReducers({
   auth: authReducer,
+  employees: employeesReducer,
 });
 
 export default REDUCERS;
--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,12 +2,16 @@ import { all, takeLatest } from 'redux-saga/effects';
 
 import * as AuthSagas from 'store/sagas/authSaga';
 import * as AuthTypes from 'store/types/authTypes';
+import * as EmployeesSagas from './employeesSaga';
+import * as EmployeesTypes from '../types/employeesTypes';
 
 function* SAGAS() {
   yield all([
     takeLatest(AuthTypes.LOGIN_USER_REQUEST_START, AuthSagas.loginUser),
     takeLatest(AuthTypes.LOGOUT_USER_REDIRECT, AuthSagas.logoutUser),
     takeLatest(AuthTypes.VALIDATE_USER_LOGIN, AuthSagas.validateLogin),
+    takeLatest(EmployeesTypes.LIST_EMPLOYEES, EmployeesSagas.listEmployees),
+    takeLatest(EmployeesTypes.CREATE_EMPLOYEE, EmployeesSagas.createEmployee),
   ]);
 }
 
